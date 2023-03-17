51fad538c07969268d0aab751235f6cd27548cf5
jenkins-admin1 <128088934+jenkins-admin1@users.noreply.github.com>
2023-03-17 16:08:24 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,28 @@
+# Configure the GCP provider
+provider "google" {
+  project = "my-project"
+  region  = "us-central1"
+}
+
+# Define the VM instance
+resource "google_compute_instance" "my-vm" {
+  name         = "my-vm"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Allocate a public IP address
+    }
+  }
+
+  metadata_startup_script = "echo 'Hello, World!'"
+}
